66417dac72b7612e249fc15f60cab8242265ee61
Josek-dev <53444783+josek-dev@users.noreply.github.com>
2020-10-02 21:46:55 +0200

    reorganize folder

---
+++ cmake-build-debug/CMakeFiles/C_Projects.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/xampp/htdocs/C-Projects/Accumulate.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Accumulate.obj"
+  "C:/xampp/htdocs/C-Projects/Caesar.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Caesar.obj"
+  "C:/xampp/htdocs/C-Projects/Coin.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Coin.obj"
+  "C:/xampp/htdocs/C-Projects/Compare.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Compare.obj"
+  "C:/xampp/htdocs/C-Projects/Find.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Find.obj"
+  "C:/xampp/htdocs/C-Projects/Letters.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Letters.obj"
+  "C:/xampp/htdocs/C-Projects/Polygonal.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Polygonal.obj"
+  "C:/xampp/htdocs/C-Projects/Quadratic.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Quadratic.obj"
+  "C:/xampp/htdocs/C-Projects/Range.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/Range.obj"
+  "C:/xampp/htdocs/C-Projects/SelectionSort.c" "C:/xampp/htdocs/C-Projects/cmake-build-debug/CMakeFiles/C_Projects.dir/SelectionSort.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/C_Projects.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Projects.dir/Accumulate.obj"
+  "CMakeFiles/C_Projects.dir/Caesar.obj"
+  "CMakeFiles/C_Projects.dir/Coin.obj"
+  "CMakeFiles/C_Projects.dir/Compare.obj"
+  "CMakeFiles/C_Projects.dir/Find.obj"
+  "CMakeFiles/C_Projects.dir/Letters.obj"
+  "CMakeFiles/C_Projects.dir/Polygonal.obj"
+  "CMakeFiles/C_Projects.dir/Quadratic.obj"
+  "CMakeFiles/C_Projects.dir/Range.obj"
+  "CMakeFiles/C_Projects.dir/SelectionSort.obj"
+  "C_Projects.exe"
+  "C_Projects.exe.manifest"
+  "C_Projects.lib"
+  "C_Projects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
